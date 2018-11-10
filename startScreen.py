4753c00596890c6ebd64e793f60d01b6365bce34
BenjaminKovacs <bach1099-btkamazonwebservices@yahoo.com>
2018-11-10 12:21:12 -0500

    start button with updated start screen

--- startScreen.py
+++ startScreen.py
@@ -2,8 +2,8 @@ import general
 import classes
 def createScreen(pygame, width, height):
     buttonSpace = 70
-    general.Button(pygame, width//2, height//3, width//4, height//20, (0,255,0), 'Start game','startGame()')
-    general.Button(pygame, width//2, height//3+buttonSpace, width//4, height//20, (255,0,0), 'Red button','startGame()')
-    general.Button(pygame, width//2, height//3+2*buttonSpace, width//4, height//20, (0,0,255), 'Blue button','startGame()')
-    general.Button(pygame, width//2, height//3+3*buttonSpace, width//4, height//20, None, 'Blank button','startGame()')
-    general.Image(pygame, width//2, (height//3-buttonSpace)//2, width//4,height//3-2*buttonSpace,'images/test.bmp')
+    #general.Button(pygame, width//2, height//3, width//4, height//20, (0,255,0), 'Start game','startGame()')
+    #general.Button(pygame, width//2, height//3+buttonSpace, width//4, height//20, (255,0,0), 'Red button','startGame()')
+    #general.Button(pygame, width//2, height//3+2*buttonSpace, width//4, height//20, (0,0,255), 'Blue button','startGame()')
+    general.Button(pygame, 276, 449, 260, 67, None, 'Blank button','startGame()')
+    general.Image(pygame, width//2, height//2, width, height, 'images/startScreen.jpg')
