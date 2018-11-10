b036ce7857297a8c9a1589384c0166d50487519b
Vincent Peng <vpeng@andrew.cmu.edu>
2018-11-09 22:53:31 -0500

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,10 +1,10 @@
 class Pet(object):
-    def __init__(self, name, animal, x, y):
+    def __init__(self, name, x, y, img):
         self.name = name
-        self.animal = animal
         self.x = x
         self.y = y
         self.hunger = 50
+        self.img = img
 
     def eat(self, food):
         self.hunger -= food
@@ -13,8 +13,31 @@ class Pet(object):
         self.x += dx
         self.y += dy
 
+    def angerLevel(self):
+        return self.hunger % 20
+
+    def getImage(self):
+        return self.img
+
+    def draw(self, surface):
+        pass
+
+class Task(object):
+    def __init__(self, task):
+        self.task = task
+
     def __repr__(self):
-        return (str(self.name), str(self.animal), self.x, self.y)
+        return str(self.task)
+
+class Statsbar(object):
+    def __init__(self, level, x, y):
+        self.level = level
+        self.x = x
+        self.y = y
+
+    def draw(self, surface):
+        pass
 
 
-        
+
+
