f0c77a06c4bb6bdd7ed6331813ea8ccd11615f7c
lhuangCMU <43707356+lhuangCMU@users.noreply.github.com>
2018-11-10 15:45:37 -0500

    text box changes

--- levelScreen.py
+++ levelScreen.py
@@ -1,7 +1,7 @@
 import general
 import classes
 import levels
-global text
+import string
 text = None
 global textDisplay
 
@@ -28,14 +28,13 @@ def textEntry(pygame, width, height, text):
     textDisplay = general.Button(pygame, 0,0,0,0, None, '', 'pass', (255,255,255))
     print('yay')
     general.Rectangle(pygame, width//2, 40*height//50, width, height//25, (0,0,0))
-    general.BlinkingRectangle(pygame, letterWidth*width//100 * len(text), 40*height//50, width//100, height//25-3, (255,255,255), 5)
+    # general.BlinkingRectangle(pygame, letterWidth*width//100 * len(text), 40*height//50, width//100, height//25-3, (255,255,255), 5)
     textDisplay.bringForward()
     textDisplay.text = text
     textDisplay.x = letterWidth*width//200 * len(text)
     textDisplay.y = 40*height//50
     textDisplay.width = letterWidth*width//100 * len(text)
     textDisplay.height = height//25-3
-    
     
 def textString(pygame, width, height, char):
     global text
@@ -45,7 +44,13 @@ def textString(pygame, width, height, char):
         text = ""
         textEntry(pygame, width, height, text)
     elif text != None:
-        text += chr(char)
-        textEntry(pygame, width, height, text)
+        # if chr(char) in ("." , "," , " ", "a" ,"b","c","d","e","f","g","h","i"):
+        if chr(char) in string.ascii_letters or chr(char) in string.digits or\
+        chr(char) == " " or chr(char) == "." or chr(char) == ",":
+            text += chr(char)
+            textEntry(pygame, width, height, text)
+        if char == 8:
+            text = text[:len(text)-1]
+            textEntry(pygame, width, height, text)
     
     
